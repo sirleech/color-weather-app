74bf4d11404f6c83e353a42c8c7a5009f04a2c9e
Christopher Lee <chris@christophersmbp.bom.gov.au>
2016-08-02 14:12:49 +1000

    add some more urls

--- get_latest_obs.sh
+++ get_latest_obs.sh
@@ -1,3 +1,10 @@
 # put this in your Cron, download the obs every 15 minutes into the app folder
 
-curl http://www.bom.gov.au/fwo/IDN60903/IDN60903.94926.json > /Users/chris/git_projects/color-weather-app/output.json
+# Sydney Obs
+curl http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.json > /Users/chris/git_projects/color-weather-app/output.json
+
+# Darwin Airport
+#curl http://www.bom.gov.au/fwo/IDD60901/IDD60901.94120.json > /Users/chris/git_projects/color-weather-app/output.json
+
+# Canberra Airport
+#curl http://www.bom.gov.au/fwo/IDN60903/IDN60903.94926.json > /Users/chris/git_projects/color-weather-app/output.json
