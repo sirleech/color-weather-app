d0447566b54d7d2e6e0b84c8f036dfc6fd471350
Christopher Lee <chris@christophersmbp.bom.gov.au>
2016-08-02 10:02:28 +1000

    add rel humidity

--- get_latest_obs.sh
+++ get_latest_obs.sh
@@ -1,1 +1,3 @@
+# put this in your Cron, download the obs every 15 minutes into the app folder
+
 curl http://www.bom.gov.au/fwo/IDN60903/IDN60903.94926.json > /Users/chris/git_projects/color-weather-app/output.json
